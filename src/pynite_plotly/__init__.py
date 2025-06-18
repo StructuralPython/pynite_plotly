9d1f8e9b87a1d4ed140f7eced17af1891d0e4758
Connor Ferster <connorferster@gmail.com>
2025-06-18 12:32:55 -0700

    feat: added plot_model(), loads now plot color according to load case

--- src/pynite_plotly/__init__.py
+++ src/pynite_plotly/__init__.py
@@ -3,4 +3,4 @@ A 3rd party visualization module for PyNiteFEA using plotly
 """
 __version__ = "0.3.0"
 
-from pynite_plotly.rendering import Renderer
+from pynite_plotly.rendering import Renderer, plot_model
